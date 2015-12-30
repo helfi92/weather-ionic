abc41d4550e8cef5b02c52e0eb28753c04df4c85
Hassan Ali <helfi92@gmail.com>
2015-12-30 15:05:00 -0500

    add controller for weather

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,6 +2,24 @@ angular.module('app.controllers',[])
 .controller('AppCtrl', function($scope,$log){
 	$log.info('AppCtrl Created');
 })
-.controller('WeatherCtrl', function($scope,$log){
+.controller('WeatherCtrl', function($scope,$log, Weather,$ionicLoading, Settings){
 	$log.info('WeatherCtrl Created');
+	$scope.haveData = false;
+	$ionicLoading.show({
+		template: 'Loading...'
+	})
+	Weather.getWeatherAtLocation(32.42, -117).then(function(resp){
+		
+		$log.info(resp);
+		$scope.current = resp.data.currently;
+		$scope.highTemp = Math.ceil(resp.data.daily.data[0].temperatureMax);
+		$scope.lowTemp = Math.ceil(resp.data.daily.data[0].temperatureMin);
+		$scope.currentTemp = Math.ceil($scope.current.temperature);
+		$scope.haveData = true;
+		$ionicLoading.hide();
+	}, function(error){
+		alert('Unable to get current conditions');
+		$log.error(error);
+	})
+	
 })
